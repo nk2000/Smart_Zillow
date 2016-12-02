5dcd481bc73388aaad4116be4265247d98ceecf4
niukai <niukai1995@gmail.com>
2016-12-02 21:45:42 +0800

    finish rpc_server and rpc_client

--- web_server/rpc_client/rpc_client_test.js
+++ web_server/rpc_client/rpc_client_test.js
@@ -3,3 +3,7 @@ var client = require('./rpc_client');
 client.add(1, 2, function(response){
   console.log('1 + 2 =' + response);
 });
+
+client.searchArea('123456', function(response){
+  console.log(response);
+});
